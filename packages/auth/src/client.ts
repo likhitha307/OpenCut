05c0cdb53adf988aedf3b76d43918a6f37d4b8b4
likhitha307 <109504967+likhitha307@users.noreply.github.com>
2025-07-16 16:20:30 +0530

    Update client.ts

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import { keys } from "./keys";
-
+#here i'm add for the practice 
 const { NEXT_PUBLIC_BETTER_AUTH_URL } = keys();
 
 export const { signIn, signUp, useSession } = createAuthClient({
