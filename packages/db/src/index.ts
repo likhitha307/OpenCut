0bd3740f49d52b5991078c8638387ff66d60362d
likhitha307 <109504967+likhitha307@users.noreply.github.com>
2025-07-16 15:46:12 +0530

    Remove the lines of code

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,9 +2,8 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
-}
+
+
 
 // Create the postgres client
 const client = postgres(process.env.DATABASE_URL);
